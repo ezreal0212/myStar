70ed56edef63b7f43193e8464d34cdcb32898ab6
ezreal0212 <snow563214@hotmail.com>
2018-05-22 13:54:55 +0800

    star app

--- myStar/ViewController.swift
+++ myStar/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var starTextField: UITextField!
+    
+    @IBOutlet weak var genderSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var resultImageView: UIImageView!
+    
+    //tap收鍵盤///
+    @IBAction func tapCloseKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    //return收鍵盤///
+    @IBAction func closeKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func pairButton(_ sender: Any) {
+       
+        if starTextField.text == "Aquarius" {
+            if genderSegmentedControl.selectedSegmentIndex == 0 {
+                resultImageView.image = UIImage(named: "yes")
+            } else {
+                resultImageView.image = UIImage(named: "no")
+            }
+        }else {
+            resultImageView.image = UIImage(named: "no")
+        }
+         view.endEditing(true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
